1def42f3cfc52cc5e028a0a7fb41df2f0384ea00
Rashit Nurzhanov <7119967@mail.ru>
2022-08-25 17:55:42 +0600

    Added connection to the PostgreSQL database

--- TaskTracker.API/Program.cs
+++ TaskTracker.API/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using TaskTracker.API.Abstractions;
+using TaskTracker.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TaskTrackerContext>(x =>
+{
+    x.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+    x.UseSnakeCaseNamingConvention();
+});
+
+builder.Services.AddScoped(typeof(DbContext), typeof(TaskTrackerContext));
+builder.Services.AddScoped(typeof(IRepository<>), typeof(EFRepository<>));
 
 var app = builder.Build();
 
