aefdebd22d252a6a5918479bf49aaa297ec8b3c2
Rashit Nurzhanov <7119967@mail.ru>
2022-08-25 18:07:46 +0600

    Task was modified

--- TaskTracker.API/Models/Entities/Task.cs
+++ TaskTracker.API/Models/Entities/Task.cs
@@ -1,9 +1,10 @@
-﻿using TaskTracker.API.Enums;
+﻿using TaskTracker.API.Abstractions;
+using TaskTracker.API.Enums;
 using TaskStatus = TaskTracker.API.Enums.TaskStatus;
 
 namespace TaskTracker.API.Models.Entities
 {
-    public class Task
+    public class Task : BaseEntity
     {
         public TaskStatus Status { get; set; }
         public string Description { get; set; }
