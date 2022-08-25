2d25814b7e73cb30718c6d3ad269fcc884b0462e
Rashit Nurzhanov <7119967@mail.ru>
2022-08-25 18:51:15 +0600

    Added a new entity Operation

---
+++ TaskTracker.API/Models/Entities/Operation.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskTracker.API.Models.Entities
+{
+    public class Operation
+    {
+        public Guid Id { get; set; }
+        public DateTime Operated { get; set; }
+        public Guid ProjectId { get; set; }
+        public Project Project { get; set; }
+        public Guid TaskId { get; set; }
+        public Task Task { get; set; }
+    }
+}
