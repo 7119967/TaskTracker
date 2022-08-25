be1748d662860ebbc1ce3c07cae747b6f1459a4b
Rashit Nurzhanov <7119967@mail.ru>
2022-08-25 20:41:39 +0600

    Added controllers for Project and Task

---
+++ TaskTracker.API/Controllers/ProjectController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskTracker.API.Abstractions;
+using TaskTracker.API.Models.Entities;
+
+namespace TaskTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly IRepository<Project> _repository;
+        public ProjectController(IRepository<Project> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Project>> Get()
+        {
+            var projects = await _repository.GetAllAsync();
+            return projects;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Project?> Get(Guid id)
+        {
+            var project = await _repository.GetAsync(id);
+            return project;
+        }
+
+        [HttpPost]
+        public async Task<Project> Post([FromBody] Project entity)
+        {
+            entity.Id = Guid.NewGuid();
+            await _repository.AddAsync(entity);
+            return entity;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(Guid id, [FromBody] Project entity)
+        {
+            var project = await _repository.GetAsync(id);
+            if (project is null)
+            {
+                return BadRequest("Project does not exist");
+            }
+
+            await _repository.UpdateAsync(entity);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var project = await _repository.GetAsync(id);
+            if (project is null)
+            {
+                return BadRequest("Project does not exist");
+            }
+
+            await _repository.DeleteAsync(id);
+            return Ok();
+        }
+    }
+}
---
+++ TaskTracker.API/Controllers/TaskController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskTracker.API.Abstractions;
+using TaskTracker.API.Models.Entities;
+using Task = TaskTracker.API.Models.Entities.Task;
+
+namespace TaskTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly IRepository<Task> _repository;
+        public TaskController(IRepository<Task> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Task>> Get()
+        {
+            var tasks = await _repository.GetAllAsync();
+            return tasks;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Task?> Get(Guid id)
+        {
+            var task = await _repository.GetAsync(id);
+            return task;
+        }
+
+        [HttpPost]
+        public async Task<Task> Post([FromBody] Task entity)
+        {
+            entity.Id = Guid.NewGuid();
+            await _repository.AddAsync(entity);
+            return entity;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(Guid id, [FromBody] Task entity)
+        {
+            var task = await _repository.GetAsync(id);
+            if (task is null)
+            {
+                return BadRequest("Task does not exist");
+            }
+
+            await _repository.UpdateAsync(entity);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var task = await _repository.GetAsync(id);
+            if (task is null)
+            {
+                return BadRequest("Task does not exist");
+            }
+
+            await _repository.DeleteAsync(id);
+            return Ok();
+        }
+    }
+}
