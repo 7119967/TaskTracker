57c4f257ddfb9cd901d218390e08f9197a659f32
Rashit Nurzhanov <7119967@mail.ru>
2022-10-30 16:16:11 +0400

    Added unittests

---
+++ TaskTracker.UnitTests/ProjectServiceTest.cs
@@ -0,0 +1,89 @@
+﻿using Moq;
+using TaskTracker.Core.Entities;
+using TaskTracker.Core.Interfaces;
+using TaskTracker.Core.Services;
+using Task = System.Threading.Tasks.Task;
+
+namespace TaskTracker.UnitTests
+{
+    public class ProjectServiceTest
+    {
+        private readonly ProjectService _projectService;
+        private readonly Mock<IUnitOfWork> _unitOfWork = new Mock<IUnitOfWork>();
+
+        public ProjectServiceTest()
+        {
+            _projectService = new ProjectService(_unitOfWork.Object);
+        }
+
+        [Fact]
+        public async Task Get_By_Id()
+        {
+            //Arange
+            var id = Guid.NewGuid();
+            var project = new Project();
+            project.Id = id;
+
+            _unitOfWork.Setup(t => t.ProjectRepository.GetById(id)).ReturnsAsync(project);
+            //Act
+            var res = await _projectService.Get(id);
+            //Asert
+            Assert.Equal(id, res.Id);
+        }
+
+        [Fact]
+        public void Get_Departament_All()
+        {
+            var objectsList = new List<Project>() {
+                new  Project{
+                Name = "Project Name 1",
+                Status = Core.Enums.ProjectStatus.NotStarted,
+                Create = DateTime.Now,
+                },
+                new  Project{
+                Name = "Project Name 2",
+                Status = Core.Enums.ProjectStatus.Active,
+                Create = DateTime.Now 
+                },
+                new  Project{
+                Name = "Project Name 3",
+                Status = Core.Enums.ProjectStatus.Completed,
+                Create = DateTime.Now
+                }
+            };
+        
+            _unitOfWork.Setup<IEnumerable<Project>>(
+                rep => rep.ProjectRepository.GetAll()
+            ).Returns((objectsList));
+
+            var res = _projectService.Gets();
+
+            Assert.NotEmpty(res);
+        }
+
+        [Fact]
+        public async Task Post_Departament()
+        {
+            try
+            {
+                Project project = new Project
+                {
+                    Name = "Project Name 1",
+                    Status = Core.Enums.ProjectStatus.NotStarted,
+                    Create = DateTime.Now
+                };
+
+                _unitOfWork.Setup(
+                   rep => rep.ProjectRepository.Add(project)
+               );
+
+                await _projectService.Insert(project);
+                Assert.NotNull(project);
+            }
+            catch (Exception ex)
+            {
+                Assert.Throws<Exception>(() => ex.Message);
+            }
+        }
+    }
+}
---
+++ TaskTracker.UnitTests/TaskServiceTest.cs
@@ -0,0 +1,90 @@
+﻿using Moq;
+using TaskTracker.Core.Interfaces;
+using TaskTracker.Core.Services;
+using MyTask = TaskTracker.Core.Entities.Task;
+using Task = System.Threading.Tasks.Task;
+
+namespace TaskTracker.UnitTests
+{
+    public class TaskServiceTest
+    {
+        private readonly TaskService _taskService;
+        private readonly Mock<IUnitOfWork> _unitOfWork = new Mock<IUnitOfWork>();
+
+        public TaskServiceTest()
+        {
+            _taskService = new TaskService(_unitOfWork.Object);
+        }
+
+        [Fact]
+        public async Task Get_By_Id()
+        {
+            //Arange
+            var id = Guid.NewGuid();
+            var task = new MyTask();
+            task.Id = id;
+
+            _unitOfWork.Setup(t => t.TaskRepository.GetById(id)).ReturnsAsync(task);
+
+            //Act
+            var res = await _taskService.Get(id);
+            //Asert
+            Assert.Equal(id, res.Id);
+        }
+
+        [Fact]
+        public void Get_Task_All()
+        {
+            var objectsList = new List<MyTask>() {
+                new  MyTask{
+                Name = "MyTask Name 1",
+                Status = Core.Enums.TaskStatus.ToDo,
+                Create = DateTime.Now,
+                },
+                new  MyTask{
+                Name = "MyTask Name 2",
+                Status = Core.Enums.TaskStatus.InProgress,
+                Create = DateTime.Now
+                },
+                new  MyTask{
+                Name = "MyTask Name 3",
+                Status = Core.Enums.TaskStatus.Done,
+                Create = DateTime.Now
+                }
+            };
+
+            _unitOfWork.Setup<IEnumerable<MyTask>>(
+                rep => rep.TaskRepository.GetAll()
+            ).Returns((objectsList));
+
+            var res = _taskService.Gets();
+
+            Assert.NotEmpty(res);
+        }
+
+        [Fact]
+        public async Task Post_Departament()
+        {
+            try
+            {
+                MyTask task = new MyTask
+                {
+                    Name = "MyTask Name 1",
+                    Status = Core.Enums.TaskStatus.ToDo,
+                    Create = DateTime.Now,
+                };
+
+                _unitOfWork.Setup(
+                   rep => rep.TaskRepository.Add(task)
+               );
+
+                await _taskService.Insert(task);
+                Assert.NotNull(task);
+            }
+            catch (Exception ex)
+            {
+                Assert.Throws<Exception>(() => ex.Message);
+            }
+        }
+    }
+}
